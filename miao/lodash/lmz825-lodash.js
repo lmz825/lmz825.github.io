4a8b9b9e51a85c2408e2867bab1f6203bf4c81bd
lmz825 <872388905@qq.com>
2020-12-08 16:40:56 +0800

    444

--- miao/lodash/lmz825-lodash.js
+++ miao/lodash/lmz825-lodash.js
@@ -1,6 +1,7 @@
 
-var lmz825 = {
-  compact: function (ary) {
+var lmz825 = function () {
+  //创建一个新数组，包含原数组中所有的非假值元素
+  function compact(ary) {
     var result = []
     for (var i = 0; i < ary.length; i++) {
       if (ary[i]) {
@@ -9,4 +10,13 @@ var lmz825 = {
     }
     return result
   }
-}
+  //将数组（array）拆分成多个 size 长度的区块，并将这些区块组成一个新数组。 如果array 无法被分割成全部等长的区块，那么最后剩余的元素将组成一个区块。
+  function chunk(array, [size]) {
+    var res = []
+    if (array.lemngth <= 0 || size <= 0) return array
+    for (i = 0; i < array.length; i = i + size) {
+      res.push(array.splice(i, i + size))
+    }
+    return res
+  }
+}()
